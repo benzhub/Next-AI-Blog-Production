cb9584f030654c72552eb9bfd4233a3ecff42d0a
Len Rich <lenrich1403@gmail.com>
2025-02-07 00:51:10 +0800

    2025/02/07

---
+++ _next/static/7-RrrPVX-UBUxpdGxbCQO/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[title]","\u002Fblog\u002Fpage\u002F[page]","\u002Fcategory\u002F[category]","\u002Fcategory\u002F[category]\u002F[page]","\u002Ftag\u002F[tag]","\u002Ftag\u002F[tag]\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
